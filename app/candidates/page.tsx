b3e28c29f5709108741966b952d1c25ab85d9309
YesImDexter <DexterS0202@gmail.com>
2026-07-18 15:28:05 +0800

    Added server.js for cpanel and placeholder pages to support npm run build

--- app/candidates/page.tsx
+++ app/candidates/page.tsx
@@ -3,8 +3,8 @@ import Header from '../components/Header';
 
 function CheckIcon() {
   return (
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="size-9">
-        <path stroke-linecap="round" stroke-linejoin="round" d="M9 12.75 11.25 15 15 9.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-9">
+        <path strokeLinecap="round" strokeLinejoin="round" d="M9 12.75 11.25 15 15 9.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
         </svg>
   );
 }
